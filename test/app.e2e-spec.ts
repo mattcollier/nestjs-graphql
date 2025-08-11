092ac00a57e3373ca277dd291de5b7190b0a85b0
Matthew Collier <collier.matthew@gmail.com>
2025-08-11 10:14:46 -0400

    Add e2e tests for the graphql API.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,43 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  describe('GrpahQL API', () => {
+    it('Gets all users', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/graphql')
+        .send({"query":"query GetUsers { users { id name email role } }"})
+        .set('Accept', 'application/json');
+      expect(response.body.data).toHaveProperty('users');
+      // expect the 5 mock users in the in-memory collection
+      expect(response.body.data.users).toHaveLength(5);
+    });
+
+    it('Successfully creates a user', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/graphql')
+        .send({"query":'mutation {createUser(createUserInput: {  email: "alice@example.com", role: ADMIN, name: "alice" }) {name role email id}}'})
+        .set('Accept', 'application/json');
+      expect(response.body.data).toHaveProperty('createUser');
+      const {createUser} = response.body.data;
+      expect(createUser).toHaveProperty("id");
+      expect(createUser).toHaveProperty("email");
+      expect(createUser).toHaveProperty("role");
+      expect(createUser).toHaveProperty("name");
+      expect(createUser.name).toStrictEqual("alice");
+    });
+
+    it('Receives an error on an invalid role value', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/graphql')
+        .send({"query":'mutation {createUser(createUserInput: {  email: "adam@example.com", role: SUPERUSER, name: "adam" }) {name role email id}}'})
+        .set('Accept', 'application/json');
+      console.log(response.body);
+      expect(response.body).toHaveProperty("errors");
+      expect(response.body.errors).toHaveLength(1);
+      const [error] = response.body.errors;
+      // check for custom error handling
+      expect(error.message).toContain('Allowed values: ADMIN, ENGINEER, USER, INTERN');
+    });
+  })
 });
